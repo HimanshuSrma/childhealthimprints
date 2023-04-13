3d2d76e33a42c9b062e0a8147701bed97219805c
Himanshu Sharma <47203633+HimanshuSrma@users.noreply.github.com>
2023-04-13 11:53:00 +0530

    changes navbar color

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
       var scroll = $(window).scrollTop();
       if (scroll > 10) {
         console.log("Window Scrolled");
-        $(".navbar").css("background", "rgba(17, 43, 68)");
+        $(".navbar").css("background", "rgba(255, 255, 255)");
         // $("a.nav-link").css("color", "#fff");
     }else {
         console.log("Window Top")
-        $(".navbar").css("background", "rgba(17, 43, 68, 0.5)");
+        $(".navbar").css("background", "rgba(255, 255, 255, 0.5)");
         // $("a.nav-link").css("color", "#000");
       }
     })
